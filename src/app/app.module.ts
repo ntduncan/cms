cbd98115706079ada0eb8240ff34468711b08b3c
ntduncan <nateduncan0@.com>
2022-01-11 12:11:47 -0700

    created contacts, contacts-list, contacts-detail, and header. Also added the the contact model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,10 @@
+
+export default class Contact{
+    constructor(
+        public id: number, 
+        public name: string, 
+        public email: string, 
+        public phone: string, 
+        public imageUrl: string, 
+        public group: string){}
+}
