773faea899855bf48d849e1632aa0e034504f6e2
ntduncan <nateduncan0@.com>
2022-01-17 23:36:47 -0700

    finished initializing contact-list. still need to complete contact-detail component

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  contacts = [
+    {id: "1",
+    name: "R. Kent Jackson",  
+    email: "jacksonk@byui.edu",  
+    phone: "208-496-3771",
+    imageUrl: "../../assets/images/jacksonk.jpg",
+    group: null},
+    {
+      id: "2",
+      name: "Rex Barzee",
+      email: "barzeer@byui.edu",
+      phone: "208-496-3768",
+      imageUrl: "../../assets/images/barzeer.jpg",
+      group: null
+    }
+  ]
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
