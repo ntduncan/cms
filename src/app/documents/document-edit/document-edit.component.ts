c8b93815c7f36f19665a3c2e9e2b1cb11bed9a9f
ntduncan <nateduncan0@.com>
2022-02-24 11:11:46 -0700

    have the add document form good to go. Just need to make compatible with edit mode

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { DocumentService } from 'src/app/document.service';
+import { WindRefService } from 'src/app/wind-ref-service.service';
 import {Document} from '../Document.model';
 
 @Component({
@@ -11,17 +15,22 @@ export class DocumentEditComponent implements OnInit {
   document: Document;
   editMode: boolean = false;
 
-  constructor() { }
+  constructor(
+    public documentService: DocumentService, 
+    private route: ActivatedRoute,
+    private windRefService: WindRefService) { }
 
   ngOnInit(): void {
+    //Initialize edit mode based on the path
   }
 
   onCancel() {
     return;
   }
 
-  onSubmit(newDocument: Document){
-    return;
+  onSubmit(form: NgForm){
+    const newDocument = new Document('1', form.value.name, form.value.url)
+    this.documentService.addDocument(newDocument);
   }
 
 }
